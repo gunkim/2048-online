c1cd9c0558481064f96c39c36f68f2f731de0231
gunkims <gunkim.dev@gmail.com>
2021-06-22 17:01:39 +0900

    레디스 설정

---
+++ backend/src/main/java/dev/gunlog/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package dev.gunlog.config;
+
+import dev.gunlog.model.Player;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        return lettuceConnectionFactory;
+    }
+    @Bean
+    public RedisTemplate<String, Player> redisTemplate() {
+        RedisTemplate<String, Player> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
