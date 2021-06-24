5017d5549e3dac2ac7b8dd148234694d65fdc848
gunkims <gunkim.dev@gmail.com>
2021-06-24 13:05:58 +0900

    웹 소켓 통신 시 jwt 토큰 인증이 가능하도록 구현

--- backend/module-auth/src/main/java/dev/gunlog/config/SecurityConfig.java
+++ backend/module-auth/src/main/java/dev/gunlog/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import dev.gunlog.provider.AsyncAuthenticationProvider;
 import dev.gunlog.provider.JwtAuthenticationProvider;
 import dev.gunlog.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -39,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .exceptionHandling()
                 .and()
@@ -66,7 +71,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     private List<String> getPermitAllPathList() {
         return Arrays.asList(
-                AUTHENTICATION_URL
+                AUTHENTICATION_URL,
+                "/webSocket/**",
+                "/play/**",
+                "/game/**"
+
         );
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
--- backend/module-socket/src/main/java/dev/gunlog/config/SocketConfig.java
+++ backend/module-socket/src/main/java/dev/gunlog/config/SocketConfig.java
@@ -1,15 +1,39 @@
 package dev.gunlog.config;
 
+import dev.gunlog.JwtAuthenticationToken;
+import dev.gunlog.filter.JwtTokenAuthenticationProcessingFilter;
+import dev.gunlog.provider.JwtAuthenticationProvider;
+import dev.gunlog.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class SocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final JwtUtil jwtUtil;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/webSocket")
@@ -20,5 +44,23 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/play");
         registry.setApplicationDestinationPrefixes("/game");
+    }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) throws BadCredentialsException {
+        registration.interceptors(new ChannelInterceptorAdapter() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String bearerToken = Optional.ofNullable(accessor.getNativeHeader(SecurityConfig.AUTHENTICATION_HEADER_NAME))
+                            .orElseThrow(() -> new BadCredentialsException("토큰 값이 없습니다.")).get(0);
+                    Jws<Claims> claimsJws = jwtUtil.parserToken(bearerToken);
+                    final UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(claimsJws.getBody().getSubject(), null);
+
+                    accessor.setUser(user);
+                }
+                return message;
+            }
+        });
     }
 }
