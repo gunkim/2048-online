834226d69b5c25ba3b37815b816d15272c59c402
gunkims <45007556+kanpann@users.noreply.github.com>
2021-07-30 23:52:56 +0900

    방 생성, 방 목록 조회 비즈니스 로직 작성

---
+++ backend/module-core/src/main/java/dev/gunlog/room/dto/RoomListResponseDto.java
@@ -0,0 +1,36 @@
+package dev.gunlog.room.dto;
+
+import dev.gunlog.room.domain.Mode;
+import dev.gunlog.room.domain.Personnel;
+import dev.gunlog.room.domain.Room;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class RoomListResponseDto {
+    private Long id;
+    private String title;
+    private String username;
+    private Mode mode;
+    private Personnel personnel;
+
+    public RoomListResponseDto(Room room) {
+        this.id = room.getId();
+        this.title = room.getTitle();
+        this.username = room.getMember().getUsername();
+        this.mode = room.getMode();
+        this.personnel = room.getPersonnel();
+    }
+    @Builder
+    public RoomListResponseDto(Long id, String title, String username, Mode mode, Personnel personnel) {
+        this.id = id;
+        this.title = title;
+        this.username = username;
+        this.mode = mode;
+        this.personnel = personnel;
+    }
+}
