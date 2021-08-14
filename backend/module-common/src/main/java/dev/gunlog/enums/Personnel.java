e9e5537310d3bc69426ed9c11d9247609fd16911
gunkims <45007556+kanpann@users.noreply.github.com>
2021-08-14 13:32:44 +0900

    모듈 공통으로 쓰기 위한 enum 작성

---
+++ backend/module-common/src/main/java/dev/gunlog/enums/Personnel.java
@@ -0,0 +1,26 @@
+package dev.gunlog.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+@Getter
+@RequiredArgsConstructor
+public enum Personnel {
+    TWO(2),
+    FOUR(4);
+
+    @JsonValue
+    private final int size;
+
+    @JsonCreator
+    public static Personnel fromString(String val) {
+        return Stream.of(values()).collect(toMap(Objects::toString, e -> e)).get(val);
+    }
+}
