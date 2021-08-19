88e8b63d7af20febe6a60f338921a86b9c5d0797
gunkims <45007556+kanpann@users.noreply.github.com>
2021-08-19 10:17:25 +0900

    컨트롤러에 있던 비즈니스 로직들을 서비스로 분리

---
+++ backend/module-socket/src/main/java/dev/gunlog/socket/SingleController.java
@@ -0,0 +1,46 @@
+package dev.gunlog.socket;
+
+import dev.gunlog.model.Game;
+import dev.gunlog.service.SingleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RequiredArgsConstructor
+@RestController
+public class SingleController {
+    private final SingleService singleService;
+
+    @MessageMapping("/start")
+    @SendTo("/play/start")
+    public Game boradCast(Principal principal) {
+        return singleService.initGame(principal.getName());
+    }
+
+    @MessageMapping("/left")
+    @SendTo("/play/left")
+    public Game leftMove(Principal principal) {
+        return singleService.leftMove(principal.getName());
+    }
+
+    @MessageMapping("/right")
+    @SendTo("/play/right")
+    public Game rightMove(Principal principal) {
+        return singleService.rightMove(principal.getName());
+    }
+
+    @MessageMapping("/top")
+    @SendTo("/play/top")
+    public Game topMove(Principal principal) {
+        return singleService.topMove(principal.getName());
+    }
+
+    @MessageMapping("/bottom")
+    @SendTo("/play/bottom")
+    public Game bottomMove(Principal principal) {
+        return singleService.bottomMove(principal.getName());
+    }
+}
