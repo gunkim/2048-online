a50d034ee47c7013132cb10c3ff4346e4182f0dc
gunkims <gunkim.dev@gmail.com>
2021-09-18 07:59:54 +0900

    refactor: radio 태그 관련 코드 정리

--- frontend/components/RoomModal.tsx
+++ frontend/components/RoomModal.tsx
@@ -9,7 +9,7 @@ const RoomModal = ({ isModalVisible, handleOk, handleCancel }) => {
   const [form, setForm] = useState({
     title: "",
     mode: "TIME_ATTACK",
-    personnel: "FOUR"
+    personnel: "TWO"
   })
 
   const handleChange = e => {
@@ -34,61 +34,60 @@ const RoomModal = ({ isModalVisible, handleOk, handleCancel }) => {
       >
         <Form {...layout}>
           <Form.Item label="제목">
-            <Input name="title" value={form.title} onChange={handleChange} />
+            <Input
+              name="title"
+              value={form.title}
+              placeholder="제목을 입력하세요"
+              onChange={handleChange}
+            />
           </Form.Item>
-          <Form.Item name="radio-group" label="인원수">
+          <Form.Item label="게임 모드">
             <Radio.Group
               name="personnel"
               value={form.personnel}
               onChange={handleChange}
             >
-              <Radio value="FOUR">4명</Radio>
               <Radio value="TWO">2명</Radio>
+              <Radio value="FOUR">4명</Radio>
             </Radio.Group>
           </Form.Item>
-          <Form.Item name="radio-group" label="게임 모드">
+          <Form.Item label="게임 모드">
             <Radio.Group name="mode" value={form.mode} onChange={handleChange}>
-              <Radio value="SPEED_ATTACK">스피드</Radio>
-              <Radio value="SURVIVAL">서바이벌</Radio>
               <Radio value="TIME_ATTACK">타임어택</Radio>
+              <Radio value="SPEED_ATTACK" disabled>
+                스피드
+              </Radio>
+              <Radio value="SURVIVAL" disabled>
+                서바이벌
+              </Radio>
             </Radio.Group>
-            <div>
-              <b>모드 설명 : </b>
-              {form.mode == "SPEED_ATTACK" ? (
-                <p>
-                  빠르게 가장 먼저{" "}
-                  <span style={{ color: "blue", fontWeight: "bold" }}>
-                    2048
-                  </span>
-                  을 만든 사람이 승리하는 모드입니다.
-                </p>
-              ) : (
-                ""
-              )}
-              {form.mode == "SURVIVAL" ? (
-                <p>
-                  <span style={{ color: "blue", fontWeight: "bold" }}>
-                    마지막
-                  </span>
-                  까지 생존한 사람이 승리하는 모드입니다.
-                </p>
-              ) : (
-                ""
-              )}
-              {form.mode == "TIME_ATTACK" ? (
-                <p>
-                  지정된 시간 안에{" "}
-                  <span style={{ color: "blue", fontWeight: "bold" }}>
-                    가장 높은 점수
-                  </span>
-                  를 기록한 사람이 승리하는 모드입니다.
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
           </Form.Item>
         </Form>
+        <div>
+          <b>모드 설명 : </b>
+          {form.mode == "SPEED_ATTACK" && (
+            <p>
+              빠르게 가장 먼저{" "}
+              <span style={{ color: "blue", fontWeight: "bold" }}>2048</span>을
+              만든 사람이 승리하는 모드입니다.
+            </p>
+          )}
+          {form.mode == "SURVIVAL" && (
+            <p>
+              <span style={{ color: "blue", fontWeight: "bold" }}>마지막</span>
+              까지 생존한 사람이 승리하는 모드입니다.
+            </p>
+          )}
+          {form.mode == "TIME_ATTACK" && (
+            <p>
+              지정된 시간 안에{" "}
+              <span style={{ color: "blue", fontWeight: "bold" }}>
+                가장 높은 점수
+              </span>
+              를 기록한 사람이 승리하는 모드입니다.
+            </p>
+          )}
+        </div>
       </Modal>
     </div>
   )
