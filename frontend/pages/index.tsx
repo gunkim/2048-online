6e293e9f9c3e20e072a443d23e73b26795d2feac
gunkims <gunkim.dev@gmail.com>
2021-10-12 15:54:31 +0900

    style: 불필요 태그

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,11 +10,10 @@ const { Title, Text } = Typography
 
 const MyTitle = styled(Title)`
   font-size: 6vh !important;
-  color: white !important;
-  color: ${props => `${props.color} !important;`};
-`
-const MyText = styled(Text)`
-  color: ${props => (props.color ? props.color : "white")};
+  a {
+    color: white !important;
+    color: ${props => `${props.color} !important;`};
+  }
 `
 const SingleIcon = styled(UserOutlined)`
   font-size: 50px;
@@ -35,43 +34,50 @@ const MySettingOutlined = styled(SettingOutlined)`
 const Tile = styled(Col)`
   min-height: ${props => props.height};
   background: ${props => props.background};
-`
-const Center = styled.div`
   padding: 50px;
 `
+const Center = styled.div``
 export default function Home() {
   return (
     <div>
       <Row>
         <Tile xs={24} lg={15} height={"600px"} background={"#413C69"}>
-          <Center>
-            <MyTitle font={30}>
-              <MultiIcon /> 멀티
-            </MyTitle>
-          </Center>
+          <MyTitle font={30}>
+            <Link href="/rooms">
+              <a>
+                <MultiIcon /> 멀티
+              </a>
+            </Link>
+          </MyTitle>
         </Tile>
         <Tile xs={24} lg={9} background={"#4A47A3"}>
-          <Center>
-            <MyTitle>
-              <SingleIcon /> 싱글
-            </MyTitle>
-          </Center>
+          <MyTitle>
+            <Link href="/single">
+              <a>
+                <SingleIcon /> 싱글
+              </a>
+            </Link>
+          </MyTitle>
         </Tile>
         <Tile xs={24} lg={15} height={"369px"} background={"#ffffff"}>
-          <Center>
-            <MyTitle color={"#413C69"}>
-              <MyTrophyOutlined /> 랭킹
-            </MyTitle>
-          </Center>
+          <MyTitle color={"#413C69"}>
+            <Link href="/single">
+              <a>
+                <MyTrophyOutlined /> 랭킹
+              </a>
+            </Link>
+          </MyTitle>
         </Tile>
         <Tile xs={24} lg={9} background={"#A7C5EB"}>
-          <Center>
-            <MyTitle>
-              <MySettingOutlined /> 내 정보
-            </MyTitle>
-          </Center>
+          <MyTitle>
+            <Link href="/single">
+              <a>
+                <MySettingOutlined /> 내 정보
+              </a>
+            </Link>
+          </MyTitle>
         </Tile>
       </Row>
     </div>
   )
-}
+};
