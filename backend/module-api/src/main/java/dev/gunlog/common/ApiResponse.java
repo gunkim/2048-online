0ae2b4de766058d5fd6cd05f1a984950bf53d831
gunkims <gunkim.dev@gmail.com>
2021-10-21 00:29:10 +0900

    refactor: 공통 response 객체 수정

--- backend/module-api/src/main/java/dev/gunlog/common/ApiResponse.java
+++ backend/module-api/src/main/java/dev/gunlog/common/ApiResponse.java
@@ -1,42 +1,89 @@
 package dev.gunlog.common;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@Setter
-@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApiResponse<T> {
-    private LocalDateTime requestTime = LocalDateTime.now();
     private String message;
-    private int statusCode;
+    private int status;
     private T data;
 
-    public ApiResponse(String message, int statusCode, T data) {
+    @Builder
+    public ApiResponse(String message, int status, T data) {
         this.message = message;
-        this.statusCode = statusCode;
+        this.status = status;
         this.data = data;
     }
-    public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<T>(message, 200, data);
+
+    private ApiResponse(final WebStatusCode code, final T errors) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.data = errors;
     }
-    public static <T> ApiResponse<T> success(String message) {
-        return new ApiResponse<T>(message, 200, null);
+    private ApiResponse(final WebStatusCode code) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
     }
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<T>("SUCCESS!", 200, data);
+        return (ApiResponse<T>) ApiResponse.builder()
+                .message(WebStatusCode.REQUEST_SUCCESS.getMessage())
+                .status(WebStatusCode.REQUEST_SUCCESS.getStatus())
+                .data(data)
+                .build();
     }
-    public static <T> ApiResponse<T> failure(String message, int statusCode, T data) {
-        return new ApiResponse<>(message, statusCode, data);
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>();
     }
-    public static <T> ApiResponse<T> failure(String message, int statusCode) {
-        return new ApiResponse<>(message, statusCode, null);
+    public static ApiResponse of(final WebStatusCode code, final BindingResult bindingResult) {
+        return new ApiResponse(code, FieldError.of(bindingResult));
     }
-    public static <T> ApiResponse<T> failure(int statusCode) {
-        return new ApiResponse<>("ERROR!", statusCode, null);
+
+    public static ApiResponse of(final WebStatusCode code) {
+        return new ApiResponse(code);
+    }
+
+    public static ApiResponse of(MethodArgumentTypeMismatchException e) {
+        final String value = e.getValue() == null ? "" : e.getValue().toString();
+        final List<ApiResponse.FieldError> errors = ApiResponse.FieldError.of(e.getName(), value, e.getErrorCode());
+        return new ApiResponse(WebStatusCode.INVALID_TYPE_VALUE, errors);
+    }
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        private FieldError(final String field, final String value, final String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
     }
 }
