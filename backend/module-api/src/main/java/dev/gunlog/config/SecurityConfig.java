c456b50f8cc57a73117754c8fb7f5cf3dddf82cc
gunkims <gunkim.dev@gmail.com>
2021-10-21 00:55:46 +0900

    refactor: Security CORS 임시로 허용

--- backend/module-api/src/main/java/dev/gunlog/config/SecurityConfig.java
+++ backend/module-api/src/main/java/dev/gunlog/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors().disable()
                 .csrf().disable()
                 .exceptionHandling()
                 .and()
