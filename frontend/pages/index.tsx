265d0b05e1009f336dfcfc6ffffd37b611da0691
gunkim <gunkim.dev@gmail.com>
2021-10-28 00:16:00 +0900

    design: layout 수정

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,13 +2,11 @@ import SettingOutlined from "@ant-design/icons/lib/icons/SettingOutlined"
 import TeamOutlined from "@ant-design/icons/lib/icons/TeamOutlined"
 import TrophyOutlined from "@ant-design/icons/lib/icons/TrophyOutlined"
 import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined"
-import { Col, Row, Typography } from "antd"
 import Link from "next/link"
 import React from "react"
 import styled from "styled-components"
-const { Title, Text } = Typography
 
-const MyTitle = styled(Title)`
+const MyTitle = styled.div`
   font-size: 3rem !important;
   a {
     color: white !important;
@@ -31,42 +29,36 @@ const MySettingOutlined = styled(SettingOutlined)`
   font-size: 50px;
   color: white;
 `
-const MyRow = styled(Row)`
+const Frame = styled.table`
+  margin: 0 auto;
+
   @media screen and (max-width: 2000px) {
-    .top {
-      height: 600px;
-    }
-    .bottom {
-      height: 369px;
-    }
+    width: 1000px;
   }
   @media screen and (max-width: 1366px) {
-    .top {
-      height: 489px;
-    }
-    .bottom {
-      height: 200px;
-    }
+    width: 600px;
   }
-`
-const Tile = styled(Col)`
-  background: ${props => props.background};
-  padding: 50px;
+  tr {
+    height: 130px;
+  }
+  td {
+    text-align: center;
+  }
 `
 export default function Home() {
   return (
-    <div>
-      <MyRow>
-        <Tile xs={24} lg={15} className="top" background={"#413C69"}>
-          <MyTitle font={30}>
+    <Frame>
+      <tr>
+        <td style={{ background: "#413C69" }}>
+          <MyTitle>
             <Link href="/rooms">
               <a>
                 <MultiIcon /> 멀티
               </a>
             </Link>
           </MyTitle>
-        </Tile>
-        <Tile xs={24} lg={9} background={"#4A47A3"}>
+        </td>
+        <td rowSpan={2} style={{ background: "#403c5e" }}>
           <MyTitle>
             <Link href="/single">
               <a>
@@ -74,17 +66,21 @@ export default function Home() {
               </a>
             </Link>
           </MyTitle>
-        </Tile>
-        <Tile xs={24} lg={15} className="bottom" background={"#ffffff"}>
-          <MyTitle color={"#413C69"}>
+        </td>
+      </tr>
+      <tr>
+        <td style={{ background: "#ffffff" }}>
+          <MyTitle color="#413C69">
             <Link href="/single">
               <a>
                 <MyTrophyOutlined /> 랭킹
               </a>
             </Link>
           </MyTitle>
-        </Tile>
-        <Tile xs={24} lg={9} background={"#A7C5EB"}>
+        </td>
+      </tr>
+      <tr>
+        <td colSpan={2} style={{ background: "#A7C5EB" }}>
           <MyTitle>
             <Link href="/single">
               <a>
@@ -92,8 +88,8 @@ export default function Home() {
               </a>
             </Link>
           </MyTitle>
-        </Tile>
-      </MyRow>
-    </div>
+        </td>
+      </tr>
+    </Frame>
   )
 }
