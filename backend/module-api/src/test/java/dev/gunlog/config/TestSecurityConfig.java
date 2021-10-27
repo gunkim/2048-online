b04807359df4a3ae8eff6a7f50b5aeb36da67227
gunkim <gunkim.dev@gmail.com>
2021-10-27 12:58:17 +0900

    test: Test용 Security Config Order 어노테이션 추가

--- backend/module-api/src/test/java/dev/gunlog/config/TestSecurityConfig.java
+++ backend/module-api/src/test/java/dev/gunlog/config/TestSecurityConfig.java
@@ -1,6 +1,7 @@
 package dev.gunlog.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
+@Order(101)
 public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
