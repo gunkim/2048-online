da92c0df2027461b505618151c4678d5f5467e5d
gunkim <gunkim.dev@gmail.com>
2021-10-27 11:10:29 +0900

    test: Controller 테스트 공통 템플릿 및 Security Test Config 정의

--- backend/module-api/src/test/java/dev/gunlog/api/member/MemberControllerTests.java
+++ backend/module-api/src/test/java/dev/gunlog/api/member/MemberControllerTests.java
@@ -1,78 +1,33 @@
 package dev.gunlog.api.member;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.gunlog.common.ApiResponse;
+import dev.gunlog.RestDocControllerTest;
 import dev.gunlog.api.member.dto.PasswordMatchResponseDto;
 import dev.gunlog.api.member.service.MemberService;
-import org.junit.jupiter.api.BeforeEach;
+import dev.gunlog.common.ApiResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 
-@ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
-@SpringBootTest
-@ActiveProfiles("dev")
-public class MemberControllerTests {
-    private MockMvc mockMvc;
-    @InjectMocks
-    private MemberController memberController;
+@Import(MemberController.class)
+public class MemberControllerTests extends RestDocControllerTest {
     @MockBean
     private MemberService memberService;
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @BeforeEach
-    public void setUp(
-            WebApplicationContext webApplicationContext,
-            RestDocumentationContextProvider restDocumentationContextProvider
-    ) {
-        this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentationContextProvider)
-                        .uris()
-                        .withScheme("http")
-                        .withHost("192.168.0.1")
-                        .withPort(8080)
-                        .and()
-                        .operationPreprocessors()
-                        .withRequestDefaults(prettyPrint())
-                        .withResponseDefaults(prettyPrint())
-                )
-                .apply(springSecurity())
-                .addFilters(new CharacterEncodingFilter("UTF-8", true))
-                .build();
-    }
-
 
     @Test
     @DisplayName("회원가입 여부 체크 테스트")
@@ -82,7 +37,7 @@ public class MemberControllerTests {
         given(memberService.checkMember(anyString())).willReturn(new PasswordMatchResponseDto(true));
 
         //when
-        final ResultActions resultActions = this.mockMvc.perform(get("/api/v2/member/check/{memberId}", memberId)).andDo(print());
+        final ResultActions resultActions = mockMvc.perform(get("/api/v2/member/check/{memberId}", memberId)).andDo(print());
 
         //then
         MvcResult result = resultActions.andExpect(status().isOk())
---
+++ backend/module-api/src/test/java/dev/gunlog/config/TestSecurityConfig.java
@@ -0,0 +1,25 @@
+package dev.gunlog.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll();
+    }
+}
