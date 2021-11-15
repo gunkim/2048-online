fa780ddc73592c88e81df056e1f30e5071737d47
gunkim <gunkim.dev@gmail.com>
2021-11-15 18:07:43 +0900

    style: Container 컴포넌트 추가

--- frontend/components/new/Layout.tsx
+++ frontend/components/new/Layout.tsx
@@ -1,8 +1,9 @@
 import { Grommet, Main } from "grommet"
 import { grommet } from "grommet/themes"
-import { ReactNode } from "react"
+import React, { ReactNode } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
+import { Container } from "@mui/material"
 
 type LayoutProps = {
   children: ReactNode
@@ -11,7 +12,9 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <Grommet theme={grommet}>
       <Header />
-      <Main>{children}</Main>
+      <Main>
+        <Container maxWidth="md">{children}</Container>
+      </Main>
       <Footer />
     </Grommet>
   )
