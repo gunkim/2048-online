40a8e376e5b9a6dfcd2c6090cb9b1e9ac08ab40f
gunkim <gunkim.dev@gmail.com>
2021-11-15 18:08:18 +0900

    design: 로그인 레이아웃 수정

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react"
+import { useRouter } from "next/router"
+import { User } from "../apis/user"
+import { signInUserAsync } from "../store/user/actions"
+import Layout from "../components/new/Layout"
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "../store"
 import {
   Box,
   Button,
-  Grommet,
-  Form,
-  FormField,
-  TextInput,
-  Notification,
-  grommet
-} from "grommet"
-import { useRouter } from "next/router"
-import { User } from "../apis/user"
-import { useDispatch } from "react-redux"
-import { signInUserAsync } from "../store/user/actions"
+  Card,
+  Grid,
+  CardActionArea,
+  CardActions,
+  Snackbar,
+  CardContent,
+  TextField,
+  Typography
+} from "@mui/material"
 
 export default function Login() {
   const dispatch = useDispatch()
@@ -22,56 +26,91 @@ export default function Login() {
     password: ""
   })
   const [isLogin, setIsLogin] = useState(false)
-  const onChange = (value: User) => {
-    setUser(value)
+  const onChange = e => {
+    const name = e.target.name
+    const value = e.target.value
+
+    setUser({
+      ...user,
+      [name]: value
+    })
   }
-  const onSubmit = async () => {
-    await dispatch(signInUserAsync.request(user))
+  const onSubmit = async e => {
+    if (e.keyCode === 13 || e.target.type === "button") {
+      await dispatch(signInUserAsync.request(user))
+    }
+  }
+  const { data } = useSelector((state: RootState) => state.user.signIn)
+  if (data) {
     setIsLogin(true)
-    router.back()
   }
   return (
-    <Grommet full theme={grommet}>
-      <Box fill align="center" justify="center">
-        <Box width="medium">
-          <Form
-            value={user}
-            onChange={nextValue => onChange(nextValue)}
-            onSubmit={onSubmit}
-          >
-            <FormField label="Username" name="username" required>
-              <TextInput
+    <Layout>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={isLogin}
+        message="로그인되었습니다!"
+      />
+      <Card
+        variant="outlined"
+        sx={{ maxWidth: 800, margin: "0 auto", padding: "20px 10px" }}
+      >
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6} component="form">
+              <Typography variant="h3" align="center">
+                로그인
+              </Typography>
+              <TextField
                 name="username"
-                type="username"
-                placeholder="유저이름을 입력해주세요"
+                variant="standard"
+                placeholder="아이디를 입력해주세요"
+                fullWidth
+                margin="normal"
+                value={user.username}
+                onChange={onChange}
               />
-            </FormField>
-            <FormField label="Password" name="password" required>
-              <TextInput
+              <br />
+              <TextField
                 name="password"
+                variant="standard"
                 type="password"
                 placeholder="비밀번호를 입력해주세요"
+                fullWidth
+                margin="normal"
+                value={user.password}
+                onChange={onChange}
+                onKeyPress={onSubmit}
               />
-            </FormField>
-            <Box direction="row" justify="between" margin={{ top: "medium" }}>
               <Button
-                type="reset"
-                label="홈으로"
-                onClick={() => router.push("/")}
-              />
-              <Button type="submit" label="로그인" primary />
-            </Box>
-          </Form>
-        </Box>
-      </Box>
-      {isLogin && (
-        <Notification
-          toast
-          status="normal"
-          title="로그인되었습니다."
-          message="환영합니다!"
-        />
-      )}
-    </Grommet>
+                type="button"
+                variant="contained"
+                fullWidth
+                onClick={onSubmit}
+              >
+                로그인
+              </Button>
+              <hr style={{ margin: "15px" }} />
+              <Button variant="outlined" fullWidth>
+                회원가입
+              </Button>
+            </Grid>
+            <Grid item xs={12} md={6} component="form">
+              <Card
+                variant="outlined"
+                sx={{
+                  maxWidth: 800,
+                  height: 274,
+                  margin: "0 auto",
+                  padding: "20px 10px"
+                }}
+              >
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Layout>
   )
 }
