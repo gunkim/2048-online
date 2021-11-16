8a58314ad3603aae059bab7464c0c3e02bd50668
gunkim <gunkim.dev@gmail.com>
2021-11-16 23:47:28 +0900

    refactor: 로그인 되어 있을 경우 사용자명 표출, Router로 이동하도록 수정

--- frontend/components/new/Header.tsx
+++ frontend/components/new/Header.tsx
@@ -1,23 +1,37 @@
 import { Anchor, Box, Header as GrommetHeader, Nav } from "grommet"
+import React, { useEffect, useState } from "react"
+import user from "../../store/user/reducer"
+import { getUsername } from "../../util/jwt-util"
+import Link from "next/link"
 
 const items = [{ label: "랭킹", href: "#" }]
 const Header = () => {
+  const [name, setName] = useState("")
+  useEffect(() => {
+    setName(getUsername())
+  }, [setName])
   return (
     <GrommetHeader background="light" pad="medium">
       <Box direction="row" align="center" gap="medium" margin="small">
-        <Anchor color="black" href="/">
-          이공사팔
-        </Anchor>
+        <Link href="/">
+          <Anchor color="black">이공사팔</Anchor>
+        </Link>
       </Box>
       <Nav direction="row">
-        {items.map(item => (
-          <Anchor
-            color="black"
-            href={item.href}
-            label={item.label}
-            key={item.label}
-          />
-        ))}
+        {name ? (
+          <Anchor color="black" label={name} />
+        ) : (
+          <>
+            {items.map(item => (
+              <Anchor
+                color="black"
+                href={item.href}
+                label={item.label}
+                key={item.label}
+              />
+            ))}
+          </>
+        )}
       </Nav>
     </GrommetHeader>
   )
