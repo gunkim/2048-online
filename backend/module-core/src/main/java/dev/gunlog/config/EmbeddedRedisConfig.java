66492255e77f245f94983ee369a139e484c5ff7c
gunkim <gunkim.dev@gmail.com>
2021-11-26 11:46:14 +0900

    refactor: profile - dev일 때만 embedded Redis가 실행되도록 수정

--- backend/module-core/src/main/java/dev/gunlog/config/EmbeddedRedisConfig.java
+++ backend/module-core/src/main/java/dev/gunlog/config/EmbeddedRedisConfig.java
@@ -1,6 +1,8 @@
 package dev.gunlog.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import redis.embedded.RedisServer;
 
@@ -8,6 +10,8 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.IOException;
 
+@Profile("dev")
+@Slf4j
 @Component
 public class EmbeddedRedisConfig {
     @Value("${spring.redis.port}")
@@ -19,6 +23,7 @@ public class EmbeddedRedisConfig {
     public void startRedis() throws IOException {
         this.redisServer = new RedisServer(redisPort);
         this.redisServer.start();
+        log.info("임베디드 Redis 시작 성공");
     }
     @PreDestroy
     public void stopRedis() {
