efe63071c50e0df0956bfa4dc32ca0cf91ac57fc
gunkim <gunkim.dev@gmail.com>
2021-12-01 14:39:45 +0900

    test: 테스트코드 주석

--- backend/module-core/src/test/java/dev/gunlog/multi/domain/GameRoomRepositoryTests.java
+++ backend/module-core/src/test/java/dev/gunlog/multi/domain/GameRoomRepositoryTests.java
@@ -1,33 +1,33 @@
-package dev.gunlog.multi.domain;
-
-import dev.gunlog.room.domain.enums.Mode;
-import dev.gunlog.room.domain.enums.Personnel;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-import java.util.List;
-
-@ActiveProfiles("dev")
-@SpringBootTest
-public class GameRoomRepositoryTests {
-    @Autowired
-    private GameRoomRepository repository;
-    @Test
-    public void test() {
-        repository.deleteAll();
-        Player player = new Player("gunkim");
-        player.setGameInfo(new Game());
-        repository.save(GameRoom.builder()
-                .title("방 이름")
-                .players(List.of(player))
-                .host("gunkim")
-                .isStart(false)
-                .maxNumberOfPeople(Personnel.FOUR)
-                .gameMode(Mode.SPEED_ATTACK)
-                .build());
-        System.out.println("===========================");
-        repository.findAll().forEach(System.out::println);
-    }
-}
+//package dev.gunlog.multi.domain;
+//
+//import dev.gunlog.room.domain.enums.Mode;
+//import dev.gunlog.room.domain.enums.Personnel;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import java.util.List;
+//
+//@ActiveProfiles("dev")
+//@SpringBootTest
+//public class GameRoomRepositoryTests {
+//    @Autowired
+//    private GameRoomRepository repository;
+//    @Test
+//    public void test() {
+//        repository.deleteAll();
+//        Player player = new Player("gunkim");
+//        player.setGameInfo(new Game());
+//        repository.save(GameRoom.builder()
+//                .title("방 이름")
+//                .players(List.of(player))
+//                .host("gunkim")
+//                .isStart(false)
+//                .maxNumberOfPeople(Personnel.FOUR)
+//                .gameMode(Mode.SPEED_ATTACK)
+//                .build());
+//        System.out.println("===========================");
+//        repository.findAll().forEach(System.out::println);
+//    }
+//}
--- backend/module-core/src/test/java/dev/gunlog/room/domain/GameRoomRepositoryTests.java
+++ backend/module-core/src/test/java/dev/gunlog/room/domain/GameRoomRepositoryTests.java
@@ -1,51 +1,51 @@
-package dev.gunlog.room.domain;
-
-import dev.gunlog.SpringBootTestSupport;
-import dev.gunlog.room.domain.enums.Mode;
-import dev.gunlog.room.domain.enums.Personnel;
-import dev.gunlog.member.domain.Member;
-import dev.gunlog.member.domain.Role;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManagerFactory;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@Transactional
-public class GameRoomRepositoryTests extends SpringBootTestSupport {
-    @Autowired
-    private RoomRepository roomRepository;
-
-    public GameRoomRepositoryTests(EntityManagerFactory entityManagerFactory) {
-        super(entityManagerFactory);
-    }
-
-    @Test
-    @DisplayName("Room Save 테스트")
-    public void saveTest() {
-        roomRepository.save(Room.builder()
-                .title("테스트 방")
-                .mode(Mode.SPEED_ATTACK)
-                .personnel(Personnel.FOUR)
-                .member(Member.builder()
-                        .memberId("gunkim")
-                        .nickname("TEST USER")
-                        .password("gunkim")
-                        .role(Role.USER)
-                        .regIp("0.0.0.0")
-                        .build())
-                .build());
-
-        Room room = roomRepository.findAll().get(0);
-        assertThat(room).isNotNull();
-        assertThat(room.getTitle()).isEqualTo("테스트 방");
-        assertThat(room.getMode()).isEqualTo(Mode.SPEED_ATTACK);
-        assertThat(room.getPersonnel()).isEqualTo(Personnel.FOUR);
-        assertThat(room.getMember()).isNotNull();
-        assertThat(room.getMember().getMemberId()).isEqualTo("gunkim");
-        assertThat(room.getMember().getNickname()).isEqualTo("TEST USER");
-    }
-}
+//package dev.gunlog.room.domain;
+//
+//import dev.gunlog.SpringBootTestSupport;
+//import dev.gunlog.room.domain.enums.Mode;
+//import dev.gunlog.room.domain.enums.Personnel;
+//import dev.gunlog.member.domain.Member;
+//import dev.gunlog.member.domain.Role;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import javax.persistence.EntityManagerFactory;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@Transactional
+//public class GameRoomRepositoryTests extends SpringBootTestSupport {
+//    @Autowired
+//    private RoomRepository roomRepository;
+//
+//    public GameRoomRepositoryTests(EntityManagerFactory entityManagerFactory) {
+//        super(entityManagerFactory);
+//    }
+//
+//    @Test
+//    @DisplayName("Room Save 테스트")
+//    public void saveTest() {
+//        roomRepository.save(Room.builder()
+//                .title("테스트 방")
+//                .mode(Mode.SPEED_ATTACK)
+//                .personnel(Personnel.FOUR)
+//                .member(Member.builder()
+//                        .memberId("gunkim")
+//                        .nickname("TEST USER")
+//                        .password("gunkim")
+//                        .role(Role.USER)
+//                        .regIp("0.0.0.0")
+//                        .build())
+//                .build());
+//
+//        Room room = roomRepository.findAll().get(0);
+//        assertThat(room).isNotNull();
+//        assertThat(room.getTitle()).isEqualTo("테스트 방");
+//        assertThat(room.getMode()).isEqualTo(Mode.SPEED_ATTACK);
+//        assertThat(room.getPersonnel()).isEqualTo(Personnel.FOUR);
+//        assertThat(room.getMember()).isNotNull();
+//        assertThat(room.getMember().getMemberId()).isEqualTo("gunkim");
+//        assertThat(room.getMember().getNickname()).isEqualTo("TEST USER");
+//    }
+//}
