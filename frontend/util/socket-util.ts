32c39c83a85ea58186d86c74cd1478eda6380287
gunkim <gunkim.dev@gmail.com>
2022-07-17 17:05:48 +0900

    Delete Frontend Project

--- frontend/util/socket-util.ts
+++
@@ -1,8 +0,0 @@
-import SockJS from "sockjs-client"
-import Stomp from "stompjs"
-
-const sockJS = new SockJS(`${process.env.NEXT_PUBLIC_SERVER_IP}/webSocket`)
-const stompClient: Stomp.Client = Stomp.over(sockJS)
-stompClient.debug = () => {}
-
-export default stompClient
