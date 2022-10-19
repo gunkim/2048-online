af9305575d83b2edd5b796fa04d2fb8af6d3cb27
gunkim <gunkim.dev@gmail.com>
2022-10-20 01:22:29 +0900

    :sparkles: Row 구현

---
+++ domain/src/main/kotlin/io/github/gunkim/game/domain/vo/Row.kt
@@ -0,0 +1,47 @@
+package io.github.gunkim.game.domain.vo
+
+data class Row(
+    private val content: List<Cell>
+) {
+    init {
+        require(content.size == SIZE) { "가로 폭은 ${SIZE}여야 합니다." }
+    }
+
+    fun moveLeft() = Row(
+        content
+            .fold(emptyList(), ::move)
+            .let(::fill)
+    )
+
+    fun moveRight() = Row(
+        content
+            .reversed()
+            .fold(emptyList(), ::move)
+            .let(::fill)
+            .reversed()
+    )
+
+    private fun fill(movedRow: List<Cell>) = if (movedRow.size < SIZE) {
+        movedRow + List(SIZE - movedRow.size) { Cell.ZERO }
+    } else {
+        movedRow
+    }
+
+    operator fun get(index: Int) = content[index]
+
+    private fun move(accumulator: List<Cell>, cell: Cell) = if (accumulator.isEmpty()) {
+        listOf(cell)
+    } else {
+        val lastCell = accumulator.last()
+
+        if (lastCell == cell) {
+            accumulator.dropLast(1) + lastCell.merge(cell)
+        } else {
+            accumulator + cell
+        }
+    }
+
+    companion object {
+        private val SIZE = 4
+    }
+}
