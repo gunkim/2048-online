4cb19e51fadda7e9c56d55faa6698911c5916922
gunkim <gunkim.dev@gmail.com>
2022-12-08 02:15:59 +0900

    :white_check_mark: Room 테스트 코드 변경

--- domain/src/test/kotlin/io/github/gunkim/game/domain/RoomTests.kt
+++ domain/src/test/kotlin/io/github/gunkim/game/domain/RoomTests.kt
@@ -2,14 +2,16 @@ package io.github.gunkim.game.domain
 
 import io.github.gunkim.game.domain.vo.MoveType
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class RoomTests {
     @Test
     fun `인원이 2명보다 적을 경우 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> { Room("test", listOf()) }
-            .apply { assertThat(message).isEqualTo("게임에 참여할 수 있는 인원은 최소 2명 이상입니다.") }
+        assertThrows<IllegalArgumentException> { Room.stop("test", listOf()) }
+            .apply { assertThat(message).isEqualTo("게임에 참여할 수 있는 인원은 최소 1명 이상입니다.") }
     }
 
     @Test
@@ -17,9 +19,12 @@ class RoomTests {
         val player1 = Player("깜지")
         val player2 = Player("짱구")
 
-        val boards = listOf(Board.create(player1), Board.create(player2))
+        val gamers = listOf(
+            Gamer(player1, Board.create(player1)),
+            Gamer(player2, Board.create(player2))
+        )
 
-        val room = Room("테스트 방", boards)
+        val room = Room.stop("테스트 방", gamers)
 
         assertThrows<IllegalStateException> { room.move(player1, MoveType.TOP) }
             .apply { assertThat(message).isEqualTo("게임이 시작되지 않았습니다.") }
@@ -30,9 +35,12 @@ class RoomTests {
         val player1 = Player("깜지")
         val player2 = Player("짱구")
 
-        val boards = listOf(Board.create(player1), Board.create(player2))
+        val gamers = listOf(
+            Gamer(player1, Board.create(player1)),
+            Gamer(player2, Board.create(player2))
+        )
 
-        val room = Room("테스트 방", boards, true)
+        val room = Room.start("테스트 방", gamers)
 
         assertThrows<IllegalStateException> { room.start(player1) }
             .apply { assertThat(message).isEqualTo("이미 게임이 시작되었습니다.") }
@@ -43,9 +51,12 @@ class RoomTests {
         val player1 = Player("깜지")
         val player2 = Player("짱구")
 
-        val boards = listOf(Board.create(player1), Board.create(player2))
+        val gamers = listOf(
+            Gamer(player1, Board.create(player1)),
+            Gamer(player2, Board.create(player2))
+        )
 
-        val room = Room("테스트 방", boards)
+        val room = Room.stop("테스트 방", gamers)
 
         assertThrows<IllegalStateException> { room.stop(player1) }
             .apply { assertThat(message).isEqualTo("게임이 시작되지 않았습니다.") }
@@ -56,9 +67,12 @@ class RoomTests {
         val player1 = Player("깜지")
         val player2 = Player("짱구")
 
-        val boards = listOf(Board.create(player1), Board.create(player2))
+        val gamers = listOf(
+            Gamer(player1, Board.create(player1)),
+            Gamer(player2, Board.create(player2))
+        )
 
-        val room = Room("테스트 방", boards)
+        val room = Room.stop("테스트 방", gamers)
 
         assertThrows<IllegalArgumentException> { room.start(player2) }
             .apply { assertThat(message).isEqualTo("시작은 방장만 할 수 있습니다.") }
@@ -69,11 +83,48 @@ class RoomTests {
         val player1 = Player("깜지")
         val player2 = Player("짱구")
 
-        val boards = listOf(Board.create(player1), Board.create(player2))
+        val gamers = listOf(
+            Gamer(player1, Board.create(player1)),
+            Gamer(player2, Board.create(player2))
+        )
 
-        val room = Room("테스트 방", boards, true)
+        val room = Room.start("테스트 방", gamers)
 
         assertThrows<IllegalArgumentException> { room.stop(player2) }
             .apply { assertThat(message).isEqualTo("종료는 방장만 할 수 있습니다.") }
+    }
+
+    @Test
+    fun `방장은 게임을 시작할 수 있다`() {
+        val player1 = Player("깜지")
+        val player2 = Player("짱구")
+
+        val gamers = listOf(
+            Gamer(player1, Board.create(player1), true),
+            Gamer(player2, Board.create(player2))
+        )
+
+        val room = Room.stop("테스트 방", gamers)
+
+        val result = room.start(player1)
+
+        assertTrue(result.isStart)
+    }
+
+    @Test
+    fun `방장은 게임을 종료할 수 있다`() {
+        val player1 = Player("깜지")
+        val player2 = Player("짱구")
+
+        val gamers = listOf(
+            Gamer(player1, Board.create(player1), true),
+            Gamer(player2, Board.create(player2))
+        )
+
+        val room = Room.start("테스트 방", gamers)
+
+        val result = room.stop(player1)
+
+        assertFalse(result.isStart)
     }
 }
