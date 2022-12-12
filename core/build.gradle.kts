48db7d1482d0fb3ac69890ea2c5f2d2c2b7ae1f6
gunkim <gunkim.dev@gmail.com>
2022-12-12 18:29:46 +0900

    :sparkles: Core 모듈 추가

---
+++ core/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+    implementation(project(":domain"))
+    implementation("org.springframework.boot:spring-boot-starter:3.0.0")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,4 @@ pluginManagement {
 }
 
 include("domain")
+include("core")
