9e4c618b759c23ea7e82be5441b5f2a97c0dc365
gunkim <gunkim.dev@gmail.com>
2022-12-12 18:31:52 +0900

    :fire: 레거시 코드 제거

--- backend/module-core/src/test/java/io/github/gunkim/Application.java
+++
@@ -1,9 +0,0 @@
-package io.github.gunkim;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@SpringBootApplication
-@EnableJpaAuditing
-public class Application {
-}
