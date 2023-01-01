d65d98e9dba08613011f1cf7c48476d73b51cab2
gunkim <gunkim.dev@gmail.com>
2023-01-02 02:30:27 +0900

    :white_check_mark: 테스트를 위한 코드 추가

--- api/src/main/kotlin/io/github/gunkim/game/application/Application.kt
+++ api/src/main/kotlin/io/github/gunkim/game/application/Application.kt
@@ -1,8 +1,15 @@
 package io.github.gunkim.game.application
 
+import io.github.gunkim.game.application.socket.room.CreateController
+import io.github.gunkim.game.domain.User
+import io.github.gunkim.game.domain.Users
+import io.github.gunkim.game.domain.vo.Social
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.stereotype.Component
+import java.util.*
 
 @ComponentScan("io.github.gunkim.game.data")
 @ComponentScan("io.github.gunkim.game.application.config")
@@ -12,4 +19,14 @@ open class Application {
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+}
+
+@Component
+class initData(
+    val users: Users
+): CommandLineRunner {
+    override fun run(vararg args: String?) {
+        CreateController.userId = users.save(User(id = UUID.randomUUID(), name = "gunkim", email = "gunkim.dev@gmail.com", social = Social.GOOGLE)).id
+    }
+
 }
