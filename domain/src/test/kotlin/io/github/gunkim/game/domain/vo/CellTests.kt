0f8ca4e67d23c1dc57d5e12c5210024d4eaebf78
gunkim <gunkim.dev@gmail.com>
2023-01-21 03:07:51 +0900

    :white_check_mark: 깨진 테스트 케이스 수정

--- domain/src/test/kotlin/io/github/gunkim/game/domain/vo/CellTests.kt
+++ domain/src/test/kotlin/io/github/gunkim/game/domain/vo/CellTests.kt
@@ -9,7 +9,7 @@ class CellTests {
     fun `최고 레벨이 레벨업을 할 경우 예외가 발생한다`() {
         val cell = Cell.TWELVE
 
-        assertThrows<IllegalStateException> { cell.merge(Cell.TWELVE) }
+        assertThrows<IllegalStateException> { cell.levelUp() }
             .apply { assertThat(message).isEqualTo("이미 최고 레벨이므로 레벨업이 불가능합니다.") }
     }
 }
