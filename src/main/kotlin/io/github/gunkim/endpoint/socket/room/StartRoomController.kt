d62e91863f95c58aae919b99de43cc38effdcf0a
gunkim <gunkim.dev@gmail.com>
2023-05-03 01:56:16 +0900

    :sparkles: 게임 시작 시 전체 플레이어에 대한 이동 처리

--- src/main/kotlin/io/github/gunkim/endpoint/socket/room/StartRoomController.kt
+++ src/main/kotlin/io/github/gunkim/endpoint/socket/room/StartRoomController.kt
@@ -2,6 +2,7 @@ package io.github.gunkim.endpoint.socket.room
 
 import io.github.gunkim.application.room.StartRoom
 import io.github.gunkim.endpoint.common.id
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
@@ -11,6 +12,7 @@ import java.util.*
 @RestController
 class StartRoomController(
     private val startRoom: StartRoom,
+    private val messagingTemplate: SimpMessagingTemplate
 ) {
     @PutMapping("/rooms/{roomId}/start")
     fun start(
@@ -18,5 +20,7 @@ class StartRoomController(
         @PathVariable roomId: UUID,
     ) {
         startRoom.start(roomId, user.id)
+
+        messagingTemplate.convertAndSend("/topic/rooms/$roomId/start", Unit)
     }
 }
--- src/main/resources/static/js/waitroom.js
+++ src/main/resources/static/js/waitroom.js
@@ -1,47 +1,43 @@
 const createPlayer = (players) => {
-  return players.map(player => {
-    return `
+    return players.map(player => {
+        return `
       <li class="player" pid="${player.id}">
         <div class="player-info">
           <div class="player-avatar"
                style="background-image: url(${player.profileImageUrl});"></div>
-          <div>${player.host ? '&#9813;':''}${player.name}</div>
+          <div>${player.host ? '&#9813;' : ''}${player.name}</div>
         </div>
         ${player.ready ? '<div class="status ready">레디</div>' : ''}
         <div class="kick">강퇴</div>
       </li>
       `;
-  });
+    });
 }
 
 const leaveRoom = async (roomId) => {
-  const response = await axios.delete(`/rooms/${roomId}/leave`);
+    const response = await axios.delete(`/rooms/${roomId}/leave`);
 
-  if(response.status === 200) {
-    window.location.href = '/';
-  }
+    if (response.status === 200) {
+        window.location.href = '/';
+    }
 }
 
 const ready = async (roomId) => {
-  await axios.put(`/rooms/${roomId}/ready`);
+    await axios.put(`/rooms/${roomId}/ready`);
 }
 
-const start = async (roomId) => {
-  await axios.put(`/rooms/${roomId}/start`);
-}
+const start = async (roomId) => await axios.put(`/rooms/${roomId}/start`);
 
-const stompClient = (() => {
-  const socket = new SockJS("/websocket");
-  const stompClient = Stomp.over(socket);
+const stompClient = createStompClient((stompClient) => {
+    stompClient.connect({}, () => {
+        stompClient.subscribe(`/topic/rooms/${roomId}/wait`, (response) => {
+            const room = JSON.parse(response.body);
 
-  stompClient.connect({}, () => {
-    stompClient.subscribe(`/topic/rooms/${roomId}/wait`, (response) => {
-      const room = JSON.parse(response.body);
-
-      putHtml('player-list', createPlayer(room.players).join(''));
-      putHtml('room-title', room.title);
+            putHtml('player-list', createPlayer(room.players).join(''));
+            putHtml('room-title', room.title);
+        });
+        stompClient.subscribe(`/topic/rooms/${roomId}/start`, () => {
+            location.href = `/rooms/${roomId}/game`;
+        });
     });
-  });
-
-  return stompClient;
-})();
+})
