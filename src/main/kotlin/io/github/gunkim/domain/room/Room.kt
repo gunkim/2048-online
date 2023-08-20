e6c7c1bf69f0c0624f696e1d9e22ef7c792ed31b
gunkim <gunkim.dev@gmail.com>
2023-08-21 00:26:15 +0900

    :recycle: 유효성 검증 로직 개선

--- src/main/kotlin/io/github/gunkim/domain/room/Room.kt
+++ src/main/kotlin/io/github/gunkim/domain/room/Room.kt
@@ -23,36 +23,20 @@ data class Room(
         require(gamers.distinctBy { it.user.id }.size == gamers.size) { "중복된 유저가 있습니다." }
     }
 
-    val hostName: String
-        get() = gamers.find(Gamer::isHost)?.user?.name ?: throw IllegalStateException("방장이 없습니다.")
-
     init {
         require(gamers.isNotEmpty()) { "게임에 참여할 수 있는 인원은 최소 1명 이상입니다." }
     }
 
     fun move(user: User, moveType: MoveType): Room {
-        if (!isStart) {
-            throw IllegalStateException("게임이 시작되지 않았습니다.")
-        }
+        check(isStart) { "게임이 시작되지 않았습니다." }
         return Room(id, title, gamers.move(user, moveType), isStart)
     }
 
     fun start(user: User): Room {
-        if (isStart) {
-            throw IllegalStateException("이미 게임이 시작되었습니다.")
-        }
-
-        if (!gamers.find(user).isHost) {
-            throw IllegalArgumentException("시작은 방장만 할 수 있습니다.")
-        }
-
-        if (gamers.size < 2) {
-            throw IllegalStateException("게임에 참여할 수 있는 인원은 최소 2명 이상입니다.")
-        }
-
-        if (!gamers.all(Gamer::isReady)) {
-            throw IllegalStateException("게임에 참여한 모든 플레이어가 준비되어야 합니다.")
-        }
+        check(!isStart) { "이미 게임이 시작되었습니다." }
+        require(gamers.find(user).isHost) { "시작은 방장만 할 수 있습니다." }
+        check(gamers.size >= 2) { "게임에 참여할 수 있는 인원은 최소 2명 이상입니다." }
+        check(gamers.all(Gamer::isReady)) { "게임에 참여한 모든 플레이어가 준비되어야 합니다." }
 
         val gamers = gamers.map(Gamer::start)
         gamers.forEachIndexed { index, gamer -> gamer.order = index }
@@ -61,13 +45,8 @@ data class Room(
     }
 
     fun stop(user: User): Room {
-        if (!isStart) {
-            throw IllegalStateException("게임이 시작되지 않았습니다.")
-        }
-
-        if (!gamers.find(user).isHost) {
-            throw IllegalArgumentException("종료는 방장만 할 수 있습니다.")
-        }
+        check(isStart) { "게임이 시작되지 않았습니다." }
+        require(gamers.find(user).isHost) { "종료는 방장만 할 수 있습니다." }
 
         return Room(id, title, gamers, false)
     }
@@ -77,30 +56,19 @@ data class Room(
         isStart = false,
     )
 
-    fun hasGamerId(gamerId: UUID) = gamers.hasId(gamerId)
     fun hasUserId(userId: UUID) = gamers.any { it.user.id == userId }
 
     fun join(user: User): Room {
-        if (isStart) {
-            throw IllegalStateException("이미 시작된 게임에는 참여할 수 없습니다.")
-        }
-
-        if (gamers.hasId(user.id)) {
-            throw IllegalArgumentException("이미 참여한 유저입니다.")
-        }
+        check(!isStart) { "이미 시작된 게임에는 참여할 수 없습니다." }
+        require(!gamers.hasId(user.id)) { "이미 참여한 유저입니다." }
 
         val gamers = gamers + Gamer(user = user, board = Board.create())
         return Room(id, title, gamers, isStart)
     }
 
     fun leave(user: User): Room {
-        if (isStart) {
-            throw IllegalStateException("이미 시작된 게임에는 나갈 수 없습니다.")
-        }
-
-        if (!gamers.hasId(user.id)) {
-            throw IllegalArgumentException("게임에 참여하지 않은 플레이어 입니다.")
-        }
+        check(!isStart) { "이미 시작된 게임에는 나갈 수 없습니다." }
+        require(gamers.hasId(user.id)) { "게임에 참여하지 않은 플레이어 입니다." }
 
         if (gamers.size == 1) {
             throw LeaveHostException()
@@ -134,9 +102,7 @@ data class Room(
     }
 
     fun ready(user: User): Room {
-        if (isStart) {
-            throw IllegalStateException("이미 시작된 게임에는 준비할 수 없습니다.")
-        }
+        check(!isStart) { "이미 시작된 게임에는 준비할 수 없습니다." }
 
         val gamers = gamers.map {
             if (it.hasPlayer(user)) {
