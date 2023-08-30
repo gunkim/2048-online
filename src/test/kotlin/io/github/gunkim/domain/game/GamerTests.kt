758eb4e0488da46b9526d3d56e25195ae2941c4f
Mang-Joo <wjwan0915@gmail.com>
2023-08-30 22:06:56 +0900

    :white_check_mark: test(Gamer) : Change hasPlayer param

--- src/test/kotlin/io/github/gunkim/domain/game/GamerTests.kt
+++ src/test/kotlin/io/github/gunkim/domain/game/GamerTests.kt
@@ -12,13 +12,13 @@ class GamerTests : StringSpec({
         val user = User(name = "gunkim", email = "gunkim.dev@gmail.com")
         val gamer = Gamer(user = user, board = Board.create())
 
-        assertTrue(gamer.hasPlayer(user))
+        assertTrue(gamer.hasPlayer(user.id))
     }
     "플레이어를 가지고 있지 않다면 거짓을 반환한다" {
         val user = User(name = "gunkim", email = "gunkim.dev@gmail.com")
         val gamer = Gamer(user = user, board = Board.create())
 
         val user2 = User(name = "gunkim2", email = "gunkim.dev@gmail.com")
-        assertFalse(gamer.hasPlayer(user2))
+        assertFalse(gamer.hasPlayer(user2.id))
     }
 })
