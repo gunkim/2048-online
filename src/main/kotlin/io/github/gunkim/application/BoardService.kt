7256eae9c399df5d32be3a8b81f2cbd050fc66d3
Mang-Joo <wjwan0915@gmail.com>
2023-08-30 22:08:11 +0900

    :sparkles: feat(BoardService) : Remove find user at user repository

--- src/main/kotlin/io/github/gunkim/application/BoardService.kt
+++ src/main/kotlin/io/github/gunkim/application/BoardService.kt
@@ -3,25 +3,22 @@ package io.github.gunkim.application
 import io.github.gunkim.domain.game.GamerRepository
 import io.github.gunkim.domain.game.MoveType
 import io.github.gunkim.domain.room.RoomRepository
-import io.github.gunkim.domain.user.UserRepository
 import org.springframework.stereotype.Service
-import java.util.*
+import java.util.UUID
 
 @Service
 class BoardService(
     private val roomRepository: RoomRepository,
-    private val userRepository: UserRepository,
     private val gamerRepository: GamerRepository,
 ) {
     fun move(roomId: UUID, userId: UUID, type: MoveType) {
         val room = roomRepository.find(roomId)
-        val user = userRepository.find(userId)
 
         if (!room.isStart) {
             error("게임이 시작되지 않았습니다.")
         }
 
-        val gamer = room.findGamer(user).move(type)
+        val gamer = room.findGamer(userId).move(type)
         gamerRepository.save(gamer)
     }
 }
