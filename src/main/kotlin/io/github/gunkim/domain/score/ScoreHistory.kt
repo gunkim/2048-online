c8705f464e2ce8c24174ee7e866dd376a80e5562
Mang-Joo <wjwan0915@gmail.com>
2023-09-20 22:10:20 +0900

    :sparkles: feat(ScoreHistory) : Rename and delete function

---
+++ src/main/kotlin/io/github/gunkim/domain/score/ScoreHistory.kt
@@ -0,0 +1,26 @@
+package io.github.gunkim.domain.score
+
+import java.util.UUID
+
+data class ScoreHistory(
+    val id: UUID = UUID.randomUUID(),
+    val userId: UUID,
+    val score: Int = 0,
+) {
+    init {
+        require(score >= 0) { "점수는 0 미만이 될 수 없습니다." }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ScoreHistory
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
