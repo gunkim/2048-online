c2d15852f981a5de0bbe2df9696b1d5151e28e7a
Mang-Joo <wjwan0915@gmail.com>
2023-09-20 22:54:59 +0900

    :sparkles: feat(ScoreHistory) : Add timestamp

--- src/main/kotlin/io/github/gunkim/domain/score/ScoreHistory.kt
+++ src/main/kotlin/io/github/gunkim/domain/score/ScoreHistory.kt
@@ -1,11 +1,14 @@
 package io.github.gunkim.domain.score
 
+import java.time.LocalDateTime
+import java.time.LocalDateTime.now
 import java.util.UUID
 
 data class ScoreHistory(
     val id: UUID = UUID.randomUUID(),
     val userId: UUID,
     val score: Int = 0,
+    val createdAt: LocalDateTime = now(),
 ) {
     init {
         require(score >= 0) { "점수는 0 미만이 될 수 없습니다." }
