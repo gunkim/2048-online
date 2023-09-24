809b119f020dbe402fd3bbc46a98208073ba40ae
Mang-Joo <wjwan0915@gmail.com>
2023-09-24 18:03:40 +0900

    :sparkles: feat : Add time in GameHistory

--- src/main/kotlin/io/github/gunkim/domain/history/GameHistory.kt
+++ src/main/kotlin/io/github/gunkim/domain/history/GameHistory.kt
@@ -1,12 +1,14 @@
 package io.github.gunkim.domain.history
 
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class GameHistory(
     val id: UUID = UUID.randomUUID(),
     val roomId: UUID,
     val userId: UUID,
-    val score: Int
+    val score: Int,
+    val createdAt: LocalDateTime = LocalDateTime.now()
 ) {
 
     override fun equals(other: Any?): Boolean {
