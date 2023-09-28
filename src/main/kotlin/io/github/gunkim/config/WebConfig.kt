8d9932f002ce4419670fe4b1c09b9d7134b6612c
gunkim <gunkim.dev@gmail.com>
2023-09-29 00:20:44 +0900

    :test_tube: Fix failing test

--- src/main/kotlin/io/github/gunkim/config/WebConfig.kt
+++ src/main/kotlin/io/github/gunkim/config/WebConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig: WebMvcConfigurer {
+class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOriginPatterns("*")
