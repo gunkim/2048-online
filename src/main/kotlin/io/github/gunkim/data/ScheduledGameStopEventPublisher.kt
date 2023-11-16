49a5064c8d99fc7391eed9476825957a02e9e9ba
gunkim <gunkim.dev@gmail.com>
2023-11-17 00:11:18 +0900

    :sparkles: 게임 종료 도메인 이벤트 발행 비즈니스 구현

---
+++ src/main/kotlin/io/github/gunkim/data/ScheduledGameStopEventPublisher.kt
@@ -0,0 +1,29 @@
+package io.github.gunkim.data
+
+import io.github.gunkim.domain.game.event.GameStopEvent
+import io.github.gunkim.domain.game.event.ScheduledGameStopNotifier
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Component
+
+@Component
+class ScheduledGameStopEventPublisher(
+    private val eventPublisher: ApplicationEventPublisher
+) : ScheduledGameStopNotifier {
+    override fun notify(event: GameStopEvent) {
+        scheduler.schedule(
+            { eventPublisher.publishEvent(event) },
+            DELAY_SECOND_TIME,
+            TimeUnit.SECONDS
+        )
+    }
+
+    companion object {
+        //TODO 게임 시간을 30초로 설정, 게임 시간을 설정하는 변수가 여기에 있는 게 맞는지는 검토해볼 필요가 있음.
+        //게임 시간이 늘어나거나 줄어들거나 혹은 동적으로 변경될 수 있기 때문임.
+        private const val DELAY_SECOND_TIME = 30L
+
+        private val scheduler = Executors.newScheduledThreadPool(4)
+    }
+}
