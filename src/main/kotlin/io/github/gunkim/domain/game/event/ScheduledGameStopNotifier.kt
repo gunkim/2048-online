f26caeaffd20d5c972b5d2b0c6148fbd6750ecd8
Mang-Joo <wjwan0915@gmail.com>
2023-11-20 23:52:21 +0900

    :sparkles: feat : Modify GameStopNotifier

--- src/main/kotlin/io/github/gunkim/domain/game/event/ScheduledGameStopNotifier.kt
+++ src/main/kotlin/io/github/gunkim/domain/game/event/ScheduledGameStopNotifier.kt
@@ -1,5 +1,7 @@
 package io.github.gunkim.domain.game.event
 
+import java.time.LocalDateTime
+
 fun interface ScheduledGameStopNotifier {
-    fun notify(event: GameStopEvent)
+    fun notify(event: GameStopEvent, endTime: LocalDateTime)
 }
